c74f6fba7e327ad544ddb07d1d67eed512966c7c
InhyeJeong <inhyejeong59@gmail.com>
2024-09-29 10:42:31 +0900

    refactor: Update getGithubData functions
    - Request commits API only if the updated_at of the repo is this year

--- src/utils/getGithubData.ts
+++ src/utils/getGithubData.ts
@@ -12,30 +12,36 @@ export async function getGithubData(usernames: string[], year?: number) {
       const orgRepos = await getOrgRepos(username)
 
       const yearData = year || new Date().getFullYear()
-
       const startDate = `${yearData}-01-01T00:00:00Z` // 시작 날짜
       const endDate = `${yearData}-12-31T23:59:59Z` // 끝 날짜
 
       const allRepos = [...repos, ...orgRepos]
 
-      const repoCommits: RepoWithCommits[] = await Promise.all(
-        allRepos.map(async (repo) => ({
-          repoName: repo.name,
-          commits: await getCommits(username, repo.name, startDate, endDate),
-          description: repo.description,
-          is_template: repo.is_template,
-          stargazers_count: repo.stargazers_count,
-          language: repo.language,
-          license: repo?.license,
-          updated_at: repo.updated_at,
-          fork: repo.fork,
-        }))
+      const repoWithCommits: RepoWithCommits[] = await Promise.all(
+        allRepos.map(async (repo) => {
+          const updatedAtYear = new Date(repo.updated_at).getFullYear()
+          const isUpdatedInCurrentYear = updatedAtYear === yearData
+
+          const commits = isUpdatedInCurrentYear ? await getCommits(username, repo.name, startDate, endDate) : []
+
+          return {
+            repoName: repo.name,
+            commits,
+            description: repo.description,
+            is_template: repo.is_template,
+            stargazers_count: repo.stargazers_count,
+            language: repo.language,
+            license: repo?.license,
+            updated_at: repo.updated_at,
+            fork: repo.fork,
+          }
+        })
       )
 
       return {
         username,
         profile,
-        repos: repoCommits,
+        repos: repoWithCommits,
       }
     })
   )
