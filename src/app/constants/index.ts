4e0f04e77007a61552e99124ec377501ece30583
InhyeJeong <inhyejeong59@gmail.com>
2024-10-01 19:05:54 +0900

    fix: Fix color constants

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -1,10 +1,9 @@
 export const COLORS = {
-  EMPTY: '#eeeeee',
-  GITHUB_1: '#d6e685',
-  GITHUB_2: '#8cc665',
-  GITHUB_3: '#44a340',
-  GITHUB_4: '#1e6823',
-  BAR_COLOR: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF'],
+  EMPTY: 'color-empty',
+  GITHUB_1: 'color-github-1',
+  GITHUB_2: 'color-github-2',
+  GITHUB_3: 'color-github-3',
+  GITHUB_4: 'color-github-4',
 }
 
 export const TITLE_CONFIG = {
